9a6de2aa6fbe8d8aa505750de295c81cf80ef69f
larynx <larynx.austrene@gmail.com>
2023-03-13 17:22:21 +0100

    Restricted bot to only some channels and dms.

--- main.py
+++ main.py
@@ -41,6 +41,13 @@ async def on_message(message):
     if message.author == client.user:  # It is our own message
         return
 
+    if message.channel.id in [1079429447368847391, 1081618378688561203, 1080949544356941874]:
+        pass
+    elif message.channel.type.name == "private":
+        pass
+    else:
+        return
+
     try:
         if "help" in message.content:
             await message.author.send(f"To add into the competition, write a message like this:\n"
@@ -58,8 +65,9 @@ async def on_message(message):
         operation = spreadsheet_connector.add_update(1, target_entry_id,
                                                      [message.author.name, cleaned_url, category, note])
 
-        await message.channel.send(
-            f"{message.author.mention} your entry into the Meta Shift Video competition has been accepted. Thank you!")
+        if message.channel.type.name != "private":
+            await message.channel.send(
+                f"{message.author.mention} your entry into the Meta Shift Video competition has been accepted. Thank you!")
 
         if operation == "added":
             await message.author.send(
