f4b211bee5e253f0a6b50e8f8599971f1c7169bd
larynx <larynx.austrene@gmail.com>
2023-03-13 16:45:18 +0100

    Moved Google Docs authentication to use a service account

--- docs.py
+++ docs.py
@@ -1,13 +1,7 @@
 import json
-import os.path
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+from apiclient import discovery
+from google.oauth2 import service_account
 
 # The ID and range of a sample spreadsheet.
 with open('secrets.json', "r") as f:
@@ -16,25 +10,10 @@ with open('secrets.json', "r") as f:
 
 class SpreadsheetConnector:
     def __init__(self):
-        creds = None
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
-
-        self.service = build('sheets', 'v4', credentials=creds)
+        scopes = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file",
+                  "https://www.googleapis.com/auth/spreadsheets"]
+        credentials = service_account.Credentials.from_service_account_file("credentials.json", scopes=scopes)
+        self.service = discovery.build('sheets', 'v4', credentials=credentials)
         self.sheet = self.service.spreadsheets()
 
     def get_ids(self, season_id):
