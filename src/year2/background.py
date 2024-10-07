4a641cff07ff120a9067e2f3a09b6c25c0381406
Larynx Austrene <larynx.austrene@gmail.com>
2024-10-07 18:33:59 +0200

    Fixed background update function

--- src/year2/background.py
+++ src/year2/background.py
@@ -24,35 +24,34 @@ async def refresh_scores(rules, max_delay):
     while True:
         refresh_window = datetime.utcnow() + max_delay / 2
 
-        for _ in range(5):
+        refresh_entries = rules.season.entries.filter(Entry.points_expiry < refresh_window)
 
-            refresh_entries = rules.season.entries.filter(Entry.points_expiry < refresh_window)
+        logger.info(f"Updating {refresh_entries.count()} entries.")
 
-            logger.info(f"Updating {refresh_entries.count()} entries.")
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
+            await rules.update(session)
 
             for entry in refresh_entries:
-                async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
-                    await rules.update(session)
+                try:
+                    score_groups, _ = await asyncio.gather(
+                        get_collated_scores(session, rules, int(entry.character_id)),
+                        asyncio.sleep(2))
+                    user_score = get_total_score(score_groups)
+                except (ValueError, AttributeError, TimeoutError, aiohttp.http_exceptions.BadHttpMessage):  # noqa
+                    await asyncio.sleep(2)  # Make sure zkill rate limit is not hit because of the error
+                    logger.warning(f"Updating character {entry.character_id} failed, skipping.", exc_info=True)
+                    continue
+                else:
+                    logger.debug(f"Entry {entry.character_id} updated to {user_score} points.")
 
-                    try:
-                        score_groups, _ = await asyncio.gather(
-                            get_collated_scores(session, rules, int(entry.character_id)),
-                            asyncio.sleep(2))
-                        user_score = get_total_score(score_groups)
-                    except (ValueError, AttributeError, TimeoutError, aiohttp.http_exceptions.BadHttpMessage):  # noqa
-                        await asyncio.sleep(2)  # Make sure zkill rate limit is not hit because of the error
-                        logger.warning(f"Updating character {entry.character_id} failed, retrying.", exc_info=True)
-                        user_score = entry.points
+                    if rules.season.end > datetime.utcnow():
+                        entry.points_expiry = datetime.utcnow() + max_delay
                     else:
-                        break
+                        entry.points_expiry = datetime.utcnow() + max_delay + (datetime.utcnow() - rules.season.end)
 
-            logger.debug(f"Entry {entry.character_id} updated to {user_score} points.")
-            if rules.season.end > datetime.utcnow():
-                entry.points_expiry = datetime.utcnow() + max_delay
-            else:
-                entry.points_expiry = datetime.utcnow() + max_delay + (datetime.utcnow() - rules.season.end)
-            entry.points = user_score
-            entry.save()
+                    entry.points = user_score
+                    entry.save()
+                    await asyncio.sleep(2)
 
-        next_refresh_time = datetime.utcnow() + max_delay / 12
-        await asyncio.sleep(max((next_refresh_time - datetime.utcnow()).total_seconds(), 0))
+    next_refresh_time = datetime.utcnow() + max_delay / 12
+    await asyncio.sleep(max((next_refresh_time - datetime.utcnow()).total_seconds(), 0))
